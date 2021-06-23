a34fb39e7b6855fd5d6c25034f1f60166260e291
Naman Patel <46571853+np453@users.noreply.github.com>
2021-06-23 21:49:16 +0530

    Update merge.cpp

--- cp/cp/merge.cpp
+++ cp/cp/merge.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 
-long long int merge(long long a[],long long tmp[],int l,int mid,int r){
+long long int mergeq(long long a[],long long tmp[],int l,int mid,int r){
     long long count=0;
     int i=l;
     int k=l;
@@ -38,7 +38,7 @@ long long int merges(long long a[],long long tmp[],int l,int r){
         mid=(l+r)/2;
         count+= merges(a,tmp,l,mid);
         count+= merges(a,tmp,mid+1,r);
-        count+= merge(a,tmp,l,mid+1,r);
+        count+= mergeq(a,tmp,l,mid+1,r);
     }
     return count;
 }
@@ -49,6 +49,44 @@ long long int inversionCount(long long arr[], long long n)//count the number swa
     return merges(arr,tmp,0,n-1);
 }
 
+
+
+ void merge(int arr[], int l, int mid, int r)
+    {
+        int n= r-l+1;
+         int tmp[n];
+         int i=l;
+         int j=0;
+         int k=mid;
+         while(i<=mid-1 && k<=r){
+             if(arr[i]<=arr[k]){
+                 tmp[j++]=arr[i++];
+             }
+             else{
+                 tmp[j++]=arr[k++];
+             }
+         }
+         while(i<=mid-1){
+             tmp[j++]=arr[i++];
+         }
+         while(k<=r){
+             tmp[j++]=arr[k++];
+         }
+         for(int i=l;i<=r;i++){
+             arr[i]=tmp[i-l];
+         }
+    }
+    void mergeSort(int arr[], int l, int r)//merge sort implementation
+    {
+        if(l<r){
+            int mid=(l+r)/2;
+            mergeSort(arr,l,mid);
+            mergeSort(arr,mid+1,r);
+            
+            merge(arr,l,mid+1,r);
+        }
+        
+    }
 
 int main(){
 return 0;
