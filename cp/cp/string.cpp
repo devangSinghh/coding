a378ccb5b50d7d3f5e78a4a8c0c19d50d7078ffa
Naman Patel <46571853+np453@users.noreply.github.com>
2021-06-29 22:15:07 +0530

    Update string.cpp

--- cp/cp/string.cpp
+++ cp/cp/string.cpp
@@ -105,6 +105,26 @@ void grayCode(int n) {
 	}
 }
 
+int romanToDecimal(string &str) {
+    map<char,int> m;
+    m['I']=1;
+    m['V']=5;
+    m['X']=10;
+    m['L']=50;
+    m['C']=100;
+    m['D']=500;
+    m['M']=1000;
+    int sum=0;
+    for(int i=0;i<str.size();i++){
+        if(m[str[i]]<m[str[i+1]]){
+            sum+=m[str[i+1]]-m[str[i]];
+            i++;
+        }
+        else sum+=m[str[i]];
+    }
+    return sum;
+}
+
 vector<vector<bool>>visitedWord;
  bool searchForThisWord(vector<vector<char>>board, string word, int i, int j, int index) {
 	if (index == word.length()) return true;
